02573b3005e88d028a05e5d70e5cbddabd57588e
nazar.popovych <nazar.popovych1918@gmail.com>
2016-12-10 20:28:31 +0200

    zpp012 USL main f. refactoring

--- src/main/java/pp/controller/Main.java
+++ src/main/java/pp/controller/Main.java
@@ -1,9 +1,7 @@
 package pp.controller;
 
 import pp.linguisticCategories.LANGUAGE;
-import pp.textFileProcessing.TextFileDivideIntoWordsService;
 import pp.textFileProcessing.TextFileImproveService;
-import pp.textFileProcessing.TextFileReadAndWriteService;
 import pp.textFileProcessing.TextFileDivideIntoWordsServiceImpl;
 import pp.textFileProcessing.TextFileImproveServiceImpl;
 import pp.textFileProcessing.TextFileReadAndWriteServiceImpl;
@@ -19,12 +17,9 @@ public class Main {
         LANGUAGE language = LANGUAGE.UKRAINIAN;
         TextFileImproveService textFileImprove = new TextFileImproveServiceImpl();
         if (textFileImprove.fileExist(pathToFile) && textFileImprove.fileIsGoodType(pathToFile)) {
-            TextFileReadAndWriteService textFileReadService = new TextFileReadAndWriteServiceImpl();
-            String textInOneString = textFileReadService.read(pathToFile);
-            TextFileDivideIntoWordsService textFileServiceImpl = new TextFileDivideIntoWordsServiceImpl();
-            List<String>  textOfFileDivideOnListWords = textFileServiceImpl.readTextFromStringAndDivideOnWordsList(textInOneString);
-            WordUniqueFormUkrainianServiceImpl parsingTextToWordsUniqueForm = new WordUniqueFormUkrainianServiceImpl();
-            parsingTextToWordsUniqueForm.parsingTextOfUniqueWords(textOfFileDivideOnListWords);
+            String textInOneString = new TextFileReadAndWriteServiceImpl().read(pathToFile);
+            List<String>  textOfFileDivideOnListWords = new TextFileDivideIntoWordsServiceImpl().readTextFromStringAndDivideOnWordsList(textInOneString);
+            new WordUniqueFormUkrainianServiceImpl().parsingTextOfUniqueWords(textOfFileDivideOnListWords);
         }
     }
 }
